ef09291d4929aecb182d6eca23815dca4d2d56ae
Lucas Neumann <lucasneumann@Lucass-MacBook-Pro.local>
2019-07-24 18:40:38 -0400

    line-height

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,19 @@ import PostList from '../components/postList'
 import HomeSection from '../components/home-section'
 import SEO from '../components/seo'
 
+const circledNumberStyle = {
+  padding: '0em 0.65em .3em',
+  borderRadius: '100%',
+  fontSize: '.5em',
+  border: '1px solid black',
+  fontWeight: '400',
+  verticalAlign: '.15rem'
+}
 const IndexPage = ({ data: { allMarkdownRemark: { edges: posts } } }) => (
   <Layout>
     <SEO title="Home" />
     <div className="vh-75">
-      <h1 className={'w-50-l pt4 pr4 f3 mb6 lh-copy black-80'}><span className={'b'} >Lucas Neumann </span>  <span className="i">(product designer),</span> has been ① helping some of the fastest-growing startups in the world ② build a culture of customer understanding, ③ design products that empower their users, and ④ grow healthy, happy design teams.<br></br><br></br> Available for freelance work and coffee in New York.⑤</h1>
+      <h1 className={'w-50-l pt4 pr4 f3 mb6 lh-copy black-80'}><span className={'b'} >Lucas Neumann </span>  <span className="i">(product designer),</span> has been <span style={circledNumberStyle}>1</span> helping some of the fastest-growing startups in the world <span style={circledNumberStyle}>2</span> build a culture of customer understanding, <span style={circledNumberStyle}>3</span> design products that empower their users, and <span style={circledNumberStyle}>4</span> grow healthy, happy design teams.<br></br><br></br> Available for freelance work and coffee in New York. <span style={circledNumberStyle}>5</span></h1>
     </div>
     <HomeSection
       posts={posts}
