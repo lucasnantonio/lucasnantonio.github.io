49003b0bd791510db55850aa310bb0ef45b276ad
Lucas Neumann <lucasneumann@Lucass-MacBook-Pro.local>
2019-10-05 19:05:38 -0400

    hover animation

--- src/components/postLink.jsx
+++ src/components/postLink.jsx
@@ -3,10 +3,22 @@ import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 function PostLink ({ post, index }) {
+  const [isHovered, setHover] = useState(false)
   return (
-    <Link className={`link black w-third flex flex-column ${index === 0 && 'mr4'}`} to={post.frontmatter.path}>
-      <Img className="w-100 br3 " fluid={post.frontmatter.cover_image.childImageSharp.fluid}></Img>
-      <p className={'neue-regular'}>{ post.frontmatter.title }</p>
+    <Link
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={
+        `link black w-third flex flex-column br4 pa3
+        `}
+      style={{ backgroundColor: isHovered ? '#f7f7f7' : 'white' }}
+      to={post.frontmatter.path}>
+      <div className="w-100 br3 overflow-hidden">
+        <Img
+          style={{ transform: `scale(${isHovered ? '1.1' : '1'})` }}
+          fluid={post.frontmatter.cover_image.childImageSharp.fluid}></Img>
+      </div>
+      <p className={'neue-regular pl2'}>{ post.frontmatter.title }</p>
     </Link>
   )
 }
--- src/components/postList.js
+++ src/components/postList.js
@@ -6,7 +6,7 @@ const PostList = ({ posts }) => {
   const list = posts
     .map((post, index) => <PostLink index={index} key={post.node.id} post={post.node} />)
 
-  return <div className={'w-100 flex'}>{list}</div>
+  return <div style={{ marginLeft: '-1rem' }} className={'w-100 flex'}>{list}</div>
 }
 
 export default PostList
