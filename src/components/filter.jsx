664925a750810352b56c4b5b458471eeba9255e3
Lucas Neumann <lucasneumann@Lucass-MBP.lan1>
2019-11-13 16:55:48 -0500

    setColors

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -3,12 +3,29 @@ import React, { useState } from "react"
 function Filter({ title }) {
   const [isHovered, setHover] = useState(false)
   const [isSelected, setSelected] = useState(false)
+
+  const getBackgroundColor = () => {
+    if (isSelected) {
+      return "#222"
+    }
+    if (isHovered) {
+      return "#f7f7f7"
+    }
+    return "#fff"
+  }
+  const getColor = () => {
+    if (isSelected) {
+      return "white"
+    }
+    return "#222"
+  }
   return (
     <li
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
+      onClick={() => setSelected(!isSelected)}
       className={`black br-pill ba b--near-white bw1 pointer pv2 ph3 neue-regular mr3 db nowrap`}
-      style={{ backgroundColor: isHovered ? "#f7f7f7" : "#ffffff" }}
+      style={{ backgroundColor: getBackgroundColor(), color: getColor() }}
     >
       {title}
     </li>
