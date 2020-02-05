5cc6fa5281eef98be6410b715533c31cdc8fbd89
lucasnantonio <lucasneumann.fau@gmail.com>
2020-02-05 18:53:21 -0500

    get random post url

--- src/components/surpriseButton.jsx
+++ src/components/surpriseButton.jsx
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react"
 import { minWidth, topics, sizes } from "./utils"
+import { Link } from "gatsby"
 
 function SurpriseButton({ posts }) {
   const [isHovered, setHover] = useState(false)
+  const [randomPost, setRandomPost] = useState("")
+
+  function startRandom() {
+    setRandomPost(getRandomPostUrl())
+  }
+  function getRandomPostUrl() {
+    return posts[Math.floor(Math.random() * posts.length)].node.frontmatter.path
+  }
 
   return (
     <>
-      <li
-        onMouseEnter={() => setHover(true)}
+      <Link
+        to={getRandomPostUrl()}
+        onMouseEnter={() => startRandom()}
         onMouseLeave={() => setHover(false)}
         onClick={() => stopColor()}
         className={`br-pill pointer pa3-l pa2-ns pa1 neue-regular mr3-l mr0 dib nowrap f5-l f6-ns f7 `}
       >
         Surprise me
-      </li>
+      </Link>
     </>
   )
 }
