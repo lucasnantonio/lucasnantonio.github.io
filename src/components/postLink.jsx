91ead23b216502b11018603b520c10190b11f359
lucasnantonio <lucasneumann.fau@gmail.com>
2020-02-05 21:30:02 -0500

    fix alignment

--- src/components/postLink.jsx
+++ src/components/postLink.jsx
@@ -21,7 +21,7 @@ function PostLink({ post, index }) {
       className={` ${
         post.frontmatter.size !== "large" ? "w-25-l w-100" : "w-50"
       }
-        link black fl pa2 flex flex-column
+        link black fl pr3 flex flex-column
         `}
       to={post.frontmatter.path}
     >
@@ -31,15 +31,15 @@ function PostLink({ post, index }) {
         fluid={post.frontmatter.cover_image.childImageSharp.fluid}
         color={post.frontmatter.color}
       />
-      <div className="black pb4 mt2 mr2">
+      <div className="black pb4 mt2 mr2 flex flex-column">
         <p
-          className={`neue-regular f5 measure-narrow mb2 fw5 ${
+          className={`neue-regular f5 measure-narrow fw5 mt2 mb1 ${
             isHovered ? "u underline" : ""
           }`}
         >
           {post.frontmatter.title}
         </p>
-        <p className={"f5 black-40 neue-regular lh-copy pt2 pv0 mb0"}>
+        <p className={"f5 black-40 neue-regular lh-copy pv0 mb0 mt0"}>
           {post.frontmatter.subtitle}
         </p>
         <div style={{ marginLeft: "-.2rem" }}>{topicTags}</div>
