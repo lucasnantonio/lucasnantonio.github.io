c5c7334f9aef6c4a4476a39b513af4b0eac1d89c
lucasnantonio <lucasneumann.fau@gmail.com>
2020-02-05 19:48:20 -0500

    encode visitor's name

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,12 +1,35 @@
 import React, { useState } from "react"
 import PhraseWithFootnote from "../components/phraseWithFootnote"
+import { useEffect } from "react"
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+// function greet () {
+
+//   if (visitor && atob(visitor)){
+//   greeting.innerHTML = "Hi there, " + capitalizeFirstLetter(atob(visitor)) + "! <br /> It's nice to have you here."
+//   presentation.innerHTML = "I am a product designer at Nubank."
+// }
+// }
 
 const Hello = () => {
+  const [visitor, setVisitor] = useState(null)
+  function getVisitor() {
+    setVisitor(window.location.search.split("?")[1])
+  }
+  useEffect(() => getVisitor())
+
   return (
     <div className="flex flex-column mb6 mt6">
       <div className="">
-        <h1 className="f3 neue-regular black tracked-tight fw5">
-          Lucas Neumann
+        <h1 className="f3 neue-regular black tracked-tight fw5 lh-title">
+          {!visitor
+            ? `Lucas Neumann`
+            : `Hi there, ${capitalizeFirstLetter(atob(visitor))}!`}
+          <br />
+          {visitor && `I'm Lucas Neumann.`}
         </h1>
         <p className="f4 measure-narrow black-40 lh-copy">
           <br /> Futurebrand, Insitum, & Nubank alumnus, now managing product
