3a1943ff0a85a8d096065590bcdc96598c225140
lucasnantonio <lucasneumann.fau@gmail.com>
2020-02-22 13:59:27 -0500

    upercase tags

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -1,12 +1,13 @@
 import React from "react"
+import { capitalize } from "./utils"
 
 function Tag({ title }) {
   return (
     <span
       key={title}
-      className="f7 pa2 ba b--black-10 dib br1 silver ml2 fl dib flex flex-column justify-center h1"
+      className="f7 pa2 b--black-10 dib br1 silver ml2 fl dib flex flex-column justify-center h1"
     >
-      <div className="mono nowrap">{title}</div>
+      <div className="mono nowrap">{capitalize(title)}</div>
     </span>
   )
 }
--- src/components/utils.js
+++ src/components/utils.js
@@ -2,3 +2,7 @@ export const minWidth = "1180px"
 export const topics = ["Product Design", "Research", "Design Ops", "Code"]
 export const sizes = ["S", "M", "L"]
 export const backgroundGray = "#f5f5f5"
+
+export function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
