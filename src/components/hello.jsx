086debd6fef445196e8f58f32a0633f02d9dcac6
lucasnantonio <lucasneumann.fau@gmail.com>
2020-03-16 12:03:43 -0400

    better hello

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -31,53 +31,8 @@ const Hello = ({ setSelectedTopics, setAll }) => {
           {visitor && `I'm Lucas Neumann.`}
         </h1>
         <p className="f2-l f3 measure-narrow black-40 lh-copy mt0">
-          <br /> I help companies{" "}
-          <span
-            className="underline-hover hover-black pointer"
-            onClick={() => setTopic("Design Ops")}
-          >
-            run design teams,
-          </span>{" "}
-          <span
-            className="underline-hover hover-black pointer"
-            onClick={() => setTopic("Research")}
-          >
-            fall in love with user research,
-          </span>{" "}
-          <span
-            className="underline-hover hover-black pointer"
-            onClick={() => setTopic("Product Design")}
-          >
-            and design empowering experiences.
-          </span>{" "}
-          <br />
-          {/* <a
-            className="link black-40 underline-hover hover-black"
-            href="http://www.futurebrand.com"
-            target="blank"
-            style={{ cursor: "ne-resize" }}
-          >
-            Futurebrand
-          </a>
-          ,{" "}
-          <a
-            className="link black-40 underline-hover hover-black"
-            href="http://www.insitum.com"
-            target="blank"
-            style={{ cursor: "ne-resize" }}
-          >
-            Insitum
-          </a>
-          ,{" "}
-          <a
-            className="link black-40 underline-hover hover-black"
-            href="http://www.kano.me"
-            target="blank"
-            style={{ cursor: "ne-resize" }}
-          >
-            Kano
-          </a>{" "}
-          &{" "} */}
+          <br />I help companies run design teams, fall in love with user
+          research, and design empowering experiences.&nbsp;
           <a
             className="link black-40 underline-hover hover-black"
             href="http://www.nubank.com.br/en"
