1772e2a8b009f2c6835430d13cac76c664d99aba
lucasnantonio <lucasneumann.fau@gmail.com>
2020-03-19 17:22:29 -0400

    footer

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 const Feedback = () => {
   return (
     <a
-      className="link black underline-hover cursor"
+      className="link black underline-hover cursor mt0-ns mt4"
       target="_blank"
       href="https://forms.gle/CTkARXSiJFT51eRAA"
     >
