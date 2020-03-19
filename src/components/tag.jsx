17f3f0412c8f87a89519ed9793980a758bf1434e
lucasnantonio <lucasneumann.fau@gmail.com>
2020-03-19 14:00:15 -0400

    brr

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -5,7 +5,7 @@ function Tag({ title }) {
   return (
     <span
       key={title}
-      className="f7 mr2 pa2 ba br-pill b--black-10 dib br1 silver fl dib flex flex-column justify-center h1"
+      className="f7 mr2 pv2  ph3 ba br-pill b--black-10 dib br1 silver fl dib flex flex-column justify-center h1"
     >
       <div className="mono nowrap">{capitalize(title)}</div>
     </span>
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,11 @@
 export const minWidth = "1180px"
-export const topics = ["Product Design", "Research", "Design Mgmt", "Code"]
+export const topics = [
+  "Product Design",
+  "Research",
+  "Design Mgmt",
+  "Code",
+  "Favorites",
+]
 export const sizes = ["S", "M", "L"]
 export const backgroundGray = "#f5f5f5"
 export const initialFadeAnimation = {
