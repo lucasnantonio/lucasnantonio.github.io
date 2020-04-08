ec8ef21f400baa18f369271337cdceb3c7228d8d
lucasnantonio <lucasneumann.fau@gmail.com>
2020-04-08 15:58:51 -0400

    images working

--- src/components/lifeEvents.js
+++ src/components/lifeEvents.js
@@ -1,11 +1,58 @@
-export const LifeEvents = [{
-    year: 2019,
-    title: "My first design memory"
-},
-{
-    year: 2018,
-    title: "My second design memory"
-}, {
-    year: 2017,
-    title: "My first design memory"
-},]
+export const LifeEvents = [
+    {
+        year: 2020,
+        title: "Design Lead @ Petal",
+        description: "I just recently packed my things in São Paulo and moved to New York. I currently lead the small but mighty team designing the future of credit."
+    },
+    {
+        year: 2019,
+        title: "Interviewed Dan Ariely on stage",
+        image: "Ariely"
+    },
+    {
+        year: 2013,
+        title: "Service Design Internship @ Insitum (Now Fjord)"
+    },
+    {
+        year: 2013,
+        title: "Student project featured in Wired.com and FastCompany"
+    },
+    {
+        year: 2013,
+        title: "UX Design Internship @ Kano Computer"
+    },
+    {
+        year: 2013,
+        title: "Exchange program @ Central Saint Martins, London"
+    },
+    {
+        year: 2012,
+        title: "EDP Design Challenge Award",
+        description: "I designed a typographic poster for school using only pictures of my hands. It went viral on DeviantArt and was recently exhibited in a gallery in the Netherlands."
+    },
+    {
+        year: 2011,
+        title: "Internship @ FutureBrand",
+        description: "My first job at a global consultancy. At the time, I did a lot of graphic and branding work, learning from the best professionals in São Paulo."
+    },
+    {
+        year: 2010,
+        title: "My first design went 'viral'",
+        description: "I designed a typographic poster for school using only pictures of my hands. It went viral on DeviantArt and was recently exhibited in a gallery in the Netherlands."
+    },
+    {
+        year: 2010,
+        title: "Started Design School in São Paulo",
+        description: "The place that made me fall in love with design. I'm grateful for having gotten such a strong generalist background."
+    },
+    {
+        year: 2008,
+        title: "My first design award",
+        description: "I learned photoshop while designing this logo for my high school graduation, and won, so everyone got swag with my work on it for the first time!"
+    },
+    {
+        year: 2008,
+        title: "My first design",
+        description: "This is the first design I can find. A logo for my high school band."
+    }
+]
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,37 +1,70 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Img from "gatsby-image/withIEPolyfill"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { LifeEvents } from "../components/lifeEvents"
 import { minWidth } from "../components/utils"
 
-const LifeEvent = ({ title, year, text }) => {
+
+
+const LifeEvent = (data, { title, year, description, image }, index) => {
+  const dotStyle = {
+    marginLeft: "-.20rem",
+    backgroundColor: "#949494",
+    height: ".5rem",
+    width: ".5rem",
+  }
+
   return (
-    <div className="flex ">
-      <div className="mr4 bl bw1"></div>
+    <div key={title} className="flex ">
+      {index !== LifeEvents.length - 1 ? (<div className="relative mr4 bl bw1 b--light-gray"></div>) : (<div className="relative mr4 bl bw1 b--white"></div>)}
+      < div style={dotStyle} className="absolute br-pill bg-red"></div>
       <div className="pa4 ba br3 bw1 b--near-white mb4">
         <div>
-          <div>{year}</div>
-          <div>{title}</div>
-          <div>{text}</div>
+          {console.log(image)}
+          {image && <Img fluid={data[image].childImageSharp.fluid}></Img>}
+          <div className="mb2">{year}</div>
+          <div className="f4 fw5 mb3">{title}</div>
+          <div className="measure lh-copy f4">{description}</div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
-const About = () => (
-  <Layout>
-    <SEO title="About me" />
-    <div
-      style={{ maxWidth: minWidth }}
-      className="flex w-100 justify-between flex-column center"
-    >
-      <h1>The story of me</h1>
-      {LifeEvents.map((item) => LifeEvent(item))}
-    </div>
-  </Layout>
-)
+const About = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="About me" />
+      <div
+        style={{ maxWidth: minWidth }}
+        className="flex w-100 justify-between flex-column center"
+      >
+        <h1>The story of me</h1>
+        {LifeEvents.map((item, index) => LifeEvent(data, item, index))}
+      </div>
+    </Layout >
+  )
+}
 
 export default About
+
+export const squareImage = graphql`
+  fragment squareImage on File {
+    childImageSharp {
+      fluid(maxWidth: 200, maxHeight: 200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    Ariely: file(relativePath: { eq: "life/ariely.jpg" }) {
+      ...squareImage
+    }
+  }
+`
