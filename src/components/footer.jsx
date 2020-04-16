ce0d06f9fdf5b0a1075f47f66acaeb9e6d972520
lucasnantonio <lucasneumann.fau@gmail.com>
2020-04-16 06:43:35 -0400

    new footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,26 +6,43 @@ import Feedback from "../components/feedback"
 const Footer = () => {
   return (
     <div
-      style={{ maxWidth: minWidth }}
-      className="flex flex-row-ns flex-column center mb6 mt3 b--near-white bw1 pt4 items-center f5-ns f6 w-100 justify-between"
+      className=" bg-near-white pv5 mt5 f4 w-100"
     >
-      <div className=" flex items-center">
-        <p className="mr3">Lucas Neumann © Always</p>
-        <a
-          className="link black underline-hover mr3"
-          href="mailto:lucasneumann.fau@gmail.com"
-        >
-          Email me
+      <div className=" ph4 flex flex-row-ns flex-column justify-between center items-start" style={{ maxWidth: minWidth }}>
+
+
+        <div className=" flex flex-column lh-copy ">
+          <p className="mt0 black-80 fw5">Lucas Neumann © Always</p>
+          <a
+            className="link black-40 hover-black-80 underline-hover mr3"
+            href="mailto:lucasneumann.fau@gmail.com"
+          >
+            Email me
         </a>
-        <a
-          target="_blank"
-          className="link black underline-hover mr3"
-          href="https://www.linkedin.com/in/lucasneumann/"
-        >
-          LinkedIn
+          <a
+            target="_blank"
+            className="link black-40 hover-black-80 underline-hover mr3"
+            href="https://www.linkedin.com/in/lucasneumann/"
+          >
+            LinkedIn
         </a>
+          <a
+            target="_blank"
+            className="link black-40 hover-black-80 underline-hover mr3"
+            href="https://medium.com/@lucasnantonio"
+          >
+            Medium
+        </a>
+          <a
+            target="_blank"
+            className="link black-40 hover-black-80 underline-hover mr3"
+            href="https://twitter.com/@lucasnantonio"
+          >
+            Twitter
+        </a>
+        </div>
+        <Feedback></Feedback>
       </div>
-      <Feedback></Feedback>
     </div>
   )
 }
