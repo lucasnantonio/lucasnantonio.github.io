6461d1e3f4c1fe1718d602dc6dfd5c30d1a13934
lucasnantonio <lucasneumann.fau@gmail.com>
2020-05-02 16:17:22 -0400

    better metadata

--- src/components/postMetadata.jsx
+++ src/components/postMetadata.jsx
@@ -6,12 +6,10 @@ export default function PostMetadata ({ frontmatter }) {
         <div className="mono fw5 black-30 mb3 pt3-l pt3 pb4 mw5 f6 lh-copy black-50 flex flex-column align-start items-start">
             {frontmatter.link && frontmatter.link_label && (
                 <a
-                    style={{ marginLeft: "-.25rem" }}
                     href={frontmatter.link}
                     target="_blank"
-                    className=" mb3 link  mt0-l  black-40 hover-black-80 pl1 br-pill f6 flex justify-between-l nowrap hover-bg-lightest-gray pointer hover-white"
-                >
-                    <span className="mr1 fw5 underline">{frontmatter.link_label}</span>
+                    className="pb1 bb b--near-white bw1 mb3 link mt0-l black-40 hover-black-80 f6 flex justify-between-l nowrap pointer hover-white"
+                >{frontmatter.link_label}
                 </a>
             )}
             <div className="">
