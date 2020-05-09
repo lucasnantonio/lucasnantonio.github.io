36422bf4e55366e4723d87ac2922a424fa91b60d
lucasnantonio <lucasneumann.fau@gmail.com>
2020-05-09 02:10:20 -0400

    motion ftw

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,32 +1,32 @@
 import React from "react"
 import { motion } from "framer-motion"
-import Code from "../images/icons/Code.svg"
-import Design from "../images/icons/Design.svg"
-import DesignOps from "../images/icons/DesignOps.svg"
-import Research from "../images/icons/Research.svg"
+import Code from "../images/icons/Code"
+import Design from "../images/icons/Design"
+import DesignOps from "../images/icons/DesignOps"
+import Research from "../images/icons/Research"
 import { minWidth } from "../components/utils"
 
 const skillList = {
   Research: {
-    image: Research,
+    image: <Research />,
     text:
       "I invest a lot of time in listening to users, advocating for their needs, collaborating with researchers, and creating workflows so that the team feels connected to who they're designing for.",
     backgroundColor: "#E8F4FF",
   },
   "Product Design": {
-    image: Design,
+    image: <Design />,
     text:
       "Understand the user, frame the problem, ideate, prototype, test, deliver, measure, repeat. I have designed products that have reached millions of people.",
     backgroundColor: "#FFF5D4",
   },
   "Design Ops": {
-    image: DesignOps,
+    image: <DesignOps />,
     text:
       "Designing design. Helping things scale at a healthy pace. Keeping the teams I work with happy, engaged and empowered.",
     backgroundColor: "#FDF5FF",
   },
   Code: {
-    image: Code,
+    image: <Code />,
     text:
       "I'm a coding begginer and enthusiast. This portfolio website is in itself an experiment in learning React. Here are some other things I've played with:",
     backgroundColor: "aliceblue",
@@ -48,32 +48,11 @@ export default function SkillSection({ skill }) {
               style={{ background: skillList[skill].backgroundColor }}
               className="h5 overflow-hidden relative w-100 center items-center pv5 ph4 flex f3 fw5 lh-copy black-40 tracked-tight "
             >
+              {skillList[skill].image}
               <div className="z-max ml7 pl6 pr5 flex flex-column">
                 {/* <h3 className="f3 fw6 mv0">{skill}</h3> */}
                 <p className="mv0 measure">{skillList[skill].text}</p>
               </div>
-              <motion.img
-                key={skillList[skill]}
-                initial={{
-                  width: 500,
-                  top: -80,
-                  left: -170,
-                  opacity: 0,
-                }}
-                animate={{
-                  width: 600,
-                  top: -150,
-                  left: -190,
-                  opacity: 1,
-                }}
-                transition={{ duration: 1 }}
-                style={{
-                  position: "absolute",
-                  // transform: "scale(1.5)",
-                  // transform: "rotate(-20deg)",
-                }}
-                src={skillList[skill].image}
-              />
             </div>
           )
         })}
--- src/components/utils.js
+++ src/components/utils.js
@@ -16,6 +16,33 @@ export const fadeInAnimation = {
   x: 0,
   opacity: 1,
 }
-export function capitalize (string) {
+export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+export const skillAnimations = {
+  container: {
+    hidden: { rotate: 10, y: 80, x: -170, opacity: 0, height: 500 },
+    show: {
+      opacity: 1,
+      y: 30,
+      x: -170,
+      height: 530,
+      rotate: 0,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.02,
+      },
+    },
+  },
+  item: {
+    // transition: { animation: "ease-in" },
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  },
+  itemPath: {
+    // transition: { animation: "ease-in" },
+    hidden: { opacity: 0, pathLength: 0 },
+    show: { opacity: 1, pathLength: 1 },
+  },
 }
