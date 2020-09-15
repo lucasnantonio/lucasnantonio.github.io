747a4fafca7ba82a004b0f3dd14819312e80a6d7
lucasnantonio <lucasneumann.fau@gmail.com>
2020-09-15 16:23:03 -0400

    animation init

--- src/components/twitterLogo.jsx
+++ src/components/twitterLogo.jsx
@@ -1,12 +1,24 @@
 import React, {useState} from 'react'
 import { ic_twitter_logo } from "../components/icons.js"
+import { motion } from 'framer-motion';
 
 export default function TwitterLogo() {
   const [isHovered, setHovered] = useState(false)
   return (
     <a target="_blank" href="http://twitter.com/lucasnantonio">
     <div>
-      <div onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}  className="pointer br-pill b--near-white ba pa3 flex bw1 br-pill h3 w3 mb0-l mb4">{ic_twitter_logo}</div>
+      <div onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}  className="overflow-hidden pointer br-pill b--near-white ba pa3 flex bw1 br-pill h3 w3 mb0-l mb4 relative hover-b--blue">
+        
+      <motion.div className="absolute bg-blue br-pill border-box"
+      style={{bottom: "0", left: "0", width: "10rem", height: "10rem"}}
+      animate={{scale: isHovered ? 1 : 0, bottom: "-5rem", left: "-5rem"}}
+      >
+      </motion.div>
+      <div className="absolute">
+      {ic_twitter_logo} 
+      </div>
+      
+        </div>
     </div>
     </a>
   )
