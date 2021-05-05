b0ebad820bd311544a82ebe197f271fb13901006
lucasnantonio <lucasneumann.fau@gmail.com>
2021-05-05 11:35:10 -0400

    tabs work

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -12,17 +12,6 @@ const Tab = ({ title, active, setTabList, tabList }) => {
     >
       <div
         key={title}
-        onClick={() => {
-          setTabList(
-            tabList.map(item => {
-              if (item.title === title) {
-                return { ...item, active: true }
-              } else {
-                return { ...item, active: false }
-              }
-            })
-          )
-        }}
         className={`pointer mr4 ${active ? "black" : "black-40"}`}
       >
         {title}
@@ -32,18 +21,23 @@ const Tab = ({ title, active, setTabList, tabList }) => {
 }
 
 const Tabs = ({ setTabList, tabList, location }) => {
-  console.log(location)
   return (
     <div style={{ maxWidth: minWidth }}>
       <div className="flex f3 fw5 tracked-tight">
         {tabList.map(item => {
+          let isActive
+          if (location === "/" && item.title.toLowerCase() === "work") {
+            isActive = true
+          } else if (location.indexOf(item.title.toLowerCase()) === 1) {
+            isActive = true
+          }
           return (
             <Tab
               key={item.title}
               setTabList={setTabList}
               tabList={tabList}
               title={item.title}
-              active={item.active}
+              active={isActive}
             ></Tab>
           )
         })}
