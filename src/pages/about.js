df1047e8abb914ee15f5ae266bf092eb45886013
lucasnantonio <lucasneumann.fau@gmail.com>
2021-05-19 09:09:54 -0400

    bettter but needs auditing

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,11 @@
 import React from "react"
 import About from "../components/about"
 import Layout from "../components/layout"
-import { minWidth, topics } from "../components/utils"
 
 const AboutPage = ({ location }) => {
   return (
     <Layout location={location}>
-      <div className=" center" style={{ maxWidth: minWidth }}>
-        <About></About>
-      </div>
+      <About></About>
     </Layout>
   )
 }
