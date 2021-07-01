2fa78a2b20d2551de091fef001c1d616769d6160
lucasnantonio <74026070+twitterbirdwatch@users.noreply.github.com>
2021-07-01 17:52:21 -0400

    rendering notes

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -3,6 +3,15 @@ import { minWidth } from "../components/utils"
 import React from "react"
 import { graphql } from "gatsby"
 
+const Note = ({ title, tags, html }) => {
+  return (
+    <div key={title}>
+      <h1>{title}</h1>
+      <div className="lh-copy" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  )
+}
+
 const NotesPage = ({
   data: {
     allMarkdownRemark: { edges: notes },
@@ -14,6 +23,17 @@ const NotesPage = ({
     <Layout location={location}>
       <div className=" center mt5 ph0-l ph4" style={{ maxWidth: minWidth }}>
         <div>mynotes</div>
+        {notes.map(item => {
+          return (
+            <>
+              <Note
+                key={item.node.frontmatter.title}
+                title={item.node.frontmatter.title}
+                html={item.node.html}
+              ></Note>
+            </>
+          )
+        })}
       </div>
     </Layout>
   )
@@ -23,7 +43,12 @@ export default NotesPage
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(filter: { fields: { collection: { eq: "notes" } } }) {
+    allMarkdownRemark(
+      filter: {
+        fields: { collection: { eq: "notes" } }
+        frontmatter: { title: { ne: "" } }
+      }
+    ) {
       totalCount
       edges {
         node {
