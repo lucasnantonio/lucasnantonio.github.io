48c8715a1c92a62e8f5438ee9c2dfd4a83a40a43
lucasnantonio <74026070+twitterbirdwatch@users.noreply.github.com>
2021-07-05 12:39:14 -0400

    working

--- src/pages/Note.js
+++ src/pages/Note.js
@@ -1,16 +1,22 @@
 import React from "react"
 import link from "../images/icons/link.svg"
+import Link from "gatsby-link"
 
-export const Note = ({ data }) => {
+export const Note = ({ data, format }) => {
   const {
+    fields,
     frontmatter: { title, tags, source },
     html,
+    excerpt,
   } = data
-  console.log(data)
-  return (
-    <div className="ba b--near-white ph4 pv3 note overflow-hidden" key={title}>
-      <h1 className="f4 fw5 black">{title}</h1>
-      {source && (
+
+  return format === "note" ? (
+    <div
+      className="link ba b--near-white ph4 pt3 pb4 note overflow-hidden"
+      key={title}
+    >
+      <h1 className="f3 fw5 black">{title}</h1>
+      {source && format === "note" && (
         <div
           style={{ width: "max-content" }}
           className="flex nowrap items-center pa1 black-50 ba br-pill b--black-10 mw6"
@@ -33,5 +39,13 @@ export const Note = ({ data }) => {
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </div>
+  ) : (
+    <Link
+      className="flex flex-column link ba b--near-white ph4 pt3 pb4 note"
+      to={`/notes/note/${title}`}
+    >
+      <h1 className="f4 fw5 black mb0">{title}</h1>
+      <div className="lh-copy f4 black-50 note-content mt3">{excerpt}</div>
+    </Link>
   )
 }
